0f99a7a66d27fb5bde2b3b43f9ab608549b66419
Gautier Moure <gautier.moure@gmail.com>
2018-02-08 16:15:21 +0100

    commit Gautier&Denis module 08/02/2018

---
+++ PPEING4/PPEING4/src/ppeing4/ModuleFerrotec.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppeing4;
+
+/**
+ *
+ * @author gautier
+ */
+public class ModuleFerrotec {
+    
+    private float seebeck;
+    private float thomson;
+    private float densite_couple;
+    private float longueur_jambe;
+    private float surface_jambe;
+    private float conduct_th;
+    private float surface_module;
+    
+    
+    
+    
+    
+    
+    
+}
+

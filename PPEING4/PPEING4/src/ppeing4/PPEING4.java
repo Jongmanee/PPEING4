44aeab5bcbcd5ed4c6853b7cfe96b1ffc4cca484
Gautier Moure <gautier.moure@gmail.com>
2018-02-15 15:18:32 +0100

    commit Gautier&Denis saisies utilisateur 15/02/2018

--- PPEING4/PPEING4/src/ppeing4/PPEING4.java
+++ PPEING4/PPEING4/src/ppeing4/PPEING4.java
@@ -5,6 +5,8 @@
  */
 package ppeing4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jongmanee Denis
@@ -17,7 +19,83 @@ public class PPEING4 {
     public static void main(String[] args) {
         // TODO code application logic here
         // HELLO WORLD BELETTE
-        int GAUTIER;
+        
+        Technologies tech1;
+        
+        Scanner sc = new Scanner(System.in);
+        float debit_m;
+        float capacite_th;
+        float diff_temperature;
+        float masse_volumique;
+        float longueur;
+        float largeur;
+        float hauteur;
+        
+        System.out.println("Veuillez saisir le débit massique :");
+         do  
+        {
+           debit_m = sc.nextFloat();
+        }while (debit_m<0);
+         
+                 System.out.println(debit_m);
+
+        
+        
+        System.out.println("Veuillez saisir la capacité massique du fluide :");
+        do  
+        {
+           capacite_th = sc.nextFloat();
+        }while (capacite_th<0);
+         
+                 System.out.println(capacite_th);
+        
+        System.out.println("Veuillez saisir la différence de température entre les deux fluides :");
+        do  
+        {
+           diff_temperature = sc.nextFloat();
+        }while (diff_temperature<0);
+         
+                 System.out.println(diff_temperature);
+        
+        System.out.println("Veuillez saisir la masse volumique du fluide :");
+        do  
+        {
+           masse_volumique = sc.nextFloat();
+        }while (masse_volumique<0);
+         
+                 System.out.println(masse_volumique);
+        
+        System.out.println("Veuillez saisir la longueur de l'espace disponible :");
+          do  
+        {
+           longueur = sc.nextFloat();
+        }while (longueur<0);
+         
+                 System.out.println(longueur);
+        System.out.println("Veuillez saisir la largeur de l'espace disponible :");
+        do  
+        {
+           largeur = sc.nextFloat();
+        }while (largeur<0);
+         
+                 System.out.println(largeur);
+        System.out.println("Veuillez saisir la hauteur de l'espace disponible :");
+        do  
+        {
+           hauteur = sc.nextFloat();
+        }while (hauteur<0);
+         
+                 System.out.println(hauteur);
+        
+        
+        
+        
+        //tech1 = new Technologies(1,0,0,0);
+        
+        
+        
+        
+        
     }
     
 }

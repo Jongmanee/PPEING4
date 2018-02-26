42c0ded9ac60d809661632a1c1171cb91f19caa0
Gautier Moure <gautier.moure@gmail.com>
2018-02-26 16:32:20 +0100

    commit Gautier Rth aprem 26/02/2018

--- PPEING4/PPEING4/src/ppeing4/Coaxial.java
+++ PPEING4/PPEING4/src/ppeing4/Coaxial.java
@@ -23,8 +23,8 @@ public class Coaxial extends Technologies{
     private double r_charge;
     private double taux_occupation;
     
-    private float resistance_th_globale;
-    private float coeff_convection_h;
+    private double resistance_th_globale;
+    private double coeff_convection_h_inverse;
     
     private float flux_thermique;
     private float puissance_electrique;
@@ -75,6 +75,128 @@ public class Coaxial extends Technologies{
         r_charge = (masse_volumique*densite_couple*taux_occupation*longueur_jambe)/(2*surface_jambe*surface_jambe);
         return r_charge;
     }
+    
+    
+    
+    public double calcul_rth (float débit_m, float masse_volumique, float epaisseur_paroi, float viscosite, float conductivite, float capacite_th)
+    {
+        
+        double l=0;
+        double h_global_inverse;
+        double A1=0;
+        double m1=0;
+        double A2=0;
+        double m2=0;
+        double h1;
+        double h2;
+        float rayon1;
+        float rayon2;
+        double debit_volumique;
+         if(hauteur<largeur)
+         {
+              rayon1=(hauteur/2)-2*epaisseur_paroi;
+              rayon2=(hauteur)-2*epaisseur_paroi;
+              l=hauteur;
+         } 
+        else
+         {
+             rayon1=(largeur/2)-2*epaisseur_paroi;
+             rayon2=(largeur)-2*epaisseur_paroi;
+             l=largeur;
+         }
+            
+        
+         
+         double Rey1;
+         double Rey2;
+         double Nuss1;
+         double Nuss2;
+         double Prandt;
+         
+         Rey1 = (4*debit_m*masse_volumique)/(Math.PI*rayon1*viscosite);
+         Rey2 = (4*debit_m*masse_volumique)/(Math.PI*rayon2*viscosite);
+       
+         Prandt = (viscosite*capacite_th)/(conductivite);  
+        
+         if(Rey1>=1 && Rey1<=4)
+         {
+             A1=0.891;
+             m1 = 0.330;
+         }
+        
+         else if(Rey1>=4 && Rey1<=40)
+         {
+             A1=0.821;
+             m1 = 0.385;
+         }
+         
+         else if(Rey1>=40 && Rey1<=4*Math.pow(10,3))
+         {
+             A1=0.615;
+             m1 = 0.466;
+         }
+         
+         else if(Rey1>=4*Math.pow(10,3) && Rey1<=4*Math.pow(10,4))
+         {
+             A1=0.175;
+             m1 = 0.618;
+         }
+         
+         else if(Rey1>=4*Math.pow(10,4) && Rey1<=4*Math.pow(10,5))
+         {
+             A1=0.024;
+             m1 = 0.805;
+         }
+         
+         
+         
+         
+         
+         if(Rey2>=1 && Rey2<=4)
+         {
+             A2=0.891;
+             m2 = 0.330;
+         }
+        
+         else if(Rey2>=4 && Rey2<=40)
+         {
+             A2=0.821;
+             m2 = 0.385;
+         }
+         
+         else if(Rey2>=40 && Rey2<=4*Math.pow(10,3))
+         {
+             A2=0.615;
+             m2 = 0.466;
+         }
+         
+         else if(Rey2>=4*Math.pow(10,3) && Rey2<=4*Math.pow(10,4))
+         {
+             A2=0.175;
+             m2 = 0.618;
+         }
+         
+         else if(Rey2>=4*Math.pow(10,4) && Rey2<=4*Math.pow(10,5))
+         {
+             A2=0.024;
+             m2 = 0.805;
+         }
+         
+         
+         
+        Nuss1= 1.11*A1*Math.pow(Rey1, m1)*Math.pow(Prandt, 0.31);
+        Nuss2= 1.11*A2*Math.pow(Rey2, m2)*Math.pow(Prandt, 0.31);
+        
+        
+        h1=(Nuss1*conductivite)/rayon1;
+        h2=(Nuss2*conductivite)/rayon2;
+        
+        coeff_convection_h_inverse = (((l/4)+epaisseur_paroi)/((l/4)*h1)) + ((((l/4)+epaisseur_paroi)*Math.log(((l/4)+epaisseur_paroi)/(l/4)))/conductivite) + (1/h2);
+        
+        resistance_th_globale= coeff_convection_h_inverse*(1/surface_contact);
+        return resistance_th_globale;
+    }
+    
     
     
     

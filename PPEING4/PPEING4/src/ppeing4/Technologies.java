b80d5a996cfa9e1e773ce910242584f8850f3a32
Gautier Moure <gautier.moure@gmail.com>
2018-02-27 10:20:01 +0100

    commit Gautier Coaxial  27/02/2018

--- PPEING4/PPEING4/src/ppeing4/Technologies.java
+++ PPEING4/PPEING4/src/ppeing4/Technologies.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package ppeing4;
+import java.lang.Math;
 
 
 /**
@@ -15,22 +16,36 @@ public class Technologies {
     protected float debit_m;
     protected float capacite_th;
     protected float diff_temperature;
+    protected float tempm;
     protected float masse_volumique;
+    protected double seebeck;
+    protected double thomson;
+    protected double densite_couple ;
+    protected double longueur_jambe = 3.45*Math.pow(10,-3);
+    protected double surface_jambe = 6.25*Math.pow(10,-6);
+    protected double conduct_th;
+    protected double surface_module = ((55*Math.pow(10,-3))*55*Math.pow(10,-3));
     
-    public Technologies (float debit_m, float capacite_th, float diff_temperature, float masse_volumique)
+    public Technologies (float debit_m, float capacite_th, float masse_volumique, float tempc, float tempf)
     {
+        this.tempm = (tempc+tempf)/2;
+        this.diff_temperature = tempc-tempf;
         this.debit_m = debit_m;
         this.capacite_th = capacite_th;
-        this.diff_temperature = diff_temperature;
         this.masse_volumique = masse_volumique;
+        
+        this.densite_couple = 
+        this.seebeck = ((22224+930.6*tempm) - (0.9905*tempm*tempm))*Math.pow(10,-9);
+        this.thomson = ((930.6-0.9905*tempm*tempm)*Math.pow(10,-9)*tempm);
+        this.conduct_th = ((62605-277.7*tempm + 0.4131*tempm*tempm)*Math.pow(10,-4));
+        
+        
         
     }
 
-    public float get_debit ()
-    {
-        return debit_m;
-    }
+   
     
+   
     
     
     

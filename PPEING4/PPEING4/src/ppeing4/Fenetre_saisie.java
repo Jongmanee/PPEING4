72c488ad7779d9c7009e027382666aa4fcb0aec7
PORTABLEMAXIME\maxim <maximelecointre@gmail.com>
2018-02-28 11:19:13 +0100

    suite

--- PPEING4/PPEING4/src/ppeing4/Fenetre_saisie.java
+++ PPEING4/PPEING4/src/ppeing4/Fenetre_saisie.java
@@ -278,17 +278,17 @@ public class Fenetre_saisie extends JFrame
            if (bulle=="Coaxial")
            {
                //faire les setVisibles
-                box1.setVisible(true);
-                box2.setVisible(true);
-                box3.setVisible(true);
-                box4.setVisible(true);
-                box5.setVisible(true);
-                box6.setVisible(true);
-                box7.setVisible(true);
-                box8.setVisible(true);
-                box9.setVisible(true);
-                box10.setVisible(true);
-                box11.setVisible(true);
+                box1.setVisible(true); //bouton
+                box2.setVisible(true); //debit
+                box3.setVisible(true); //capacite thermique
+                box4.setVisible(true); //temp chaud
+                box5.setVisible(true); //temp froid
+                box6.setVisible(true); //longueur
+                box7.setVisible(true); //largeur
+                box8.setVisible(true); //hauteur
+                box9.setVisible(true); //viscosité
+                box10.setVisible(true); //epaisseur
+                box11.setVisible(true); //masse volumique
            }
            if (bulle=="À plaques")
            {

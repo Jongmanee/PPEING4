6c7b742b167166ffa64c5769cd9714064abfa6c5
PORTABLEMAXIME\maxim <maximelecointre@gmail.com>
2018-04-11 09:58:50 +0200

    clean final

--- PPEING4/PPEING4/src/ppeing4/PPEING4.java
+++ PPEING4/PPEING4/src/ppeing4/PPEING4.java
@@ -5,10 +5,6 @@
  */
 package ppeing4;
 
-import java.util.Scanner;
-
-
-
 /**
  *
  * @author Jongmanee Denis
@@ -21,7 +17,6 @@ public class PPEING4 {
     public static void main(String[] args) 
     {
         // TODO code application logic here     
-     
         Ecran fenetre;
         fenetre = new Ecran();
         fenetre.setVisible(true);
